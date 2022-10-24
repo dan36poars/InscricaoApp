76bec9f79a4451e376433c62c7f8de6f3e92d914
Daniel Corrêa <eldestemido@gmail.com>
2022-10-24 12:00:39 +0000

    chore: Create page structure

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/home/index', to: 'home#index'
-
   root 'home#index'
+  resources :subscripts, only: [:new]
 end
