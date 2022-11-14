41a9ffe8729d3013930812e87176fcda1fe7e9ac
Daniel Corrêa <eldestemido@gmail.com>
2022-11-14 17:03:53 +0000

    feat: add queue subscript

--- app/controllers/api/v1/subscripts_controller.rb
+++ app/controllers/api/v1/subscripts_controller.rb
@@ -4,11 +4,8 @@ class Api::V1::SubscriptsController < ApplicationController
   end
 
   def create
-    @subscript = Subscript.new(subscript_params)
-
-    if @subscript.save
-      render json: @subscript, status: :created      
-    end
+    SubscriptCreateJob.perform_later(subscript_params)
+    render json: { message: "subscript was send success" }, :status => :created
   end
 
   private
